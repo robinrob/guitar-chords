3045d6b453ad5bdc4683335d32a8fbae0a2ae888
Robin Smith <robin@rsmith.io>
2017-11-16 14:22:53 +0000

    Interface working

--- GuitarChords/ViewController.swift
+++ GuitarChords/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var e1: [UIButton]!
+    @IBOutlet var a: [UIButton]!
+    @IBOutlet var d: [UIButton]!
+    @IBOutlet var g: [UIButton]!
+    @IBOutlet var b: [UIButton]!
+    @IBOutlet var e2: [UIButton]!
+//
+    var strings: [[UIButton]] {
+        get {
+//            return [e1, a, d, g, b, e2]
+            return [e1]
+        }
+    }
+//
+//    var isCSelected: Bool = false
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        initStrings()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,68 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showChord(_ sender: UIButton) {
+        if !sender.isSelected {
+            sender.isSelected = true
+            let name = sender.currentTitle!
+            displayChord(name)
+        } else {
+            sender.isSelected = false
+            clearAllStrings()
+        }
+    }
 
+    func displayChord(_ name: String) {
+        print("here")
+        if name == "C" {
+            displayCChord()
+        } else {
+            print(strings)
+            for string in strings {
+                for fret in string {
+                    fret.backgroundColor = UIColor.red
+                }
+            }
+        }
+    }
+
+    func displayCChord() {
+//        for fret in e1 {
+//            fret.backgroundColor = UIColor.gray
+//        }
+//        selectFret(a, 3)
+//        selectFret(d, 2)
+//        selectFret(b, 1)
+
+        print(e1)
+        selectFret(e1, 14)
+//        selectFret(a, 1)
+//        selectFret(d, 1)
+//        selectFret(g, 1)
+//        selectFret(b, 1)
+//        selectFret(e2, 1)
+    }
+
+    func selectFret(_ string: [UIButton], _ fretNum: Int) {
+        print(fretNum)
+        string[fretNum-1].backgroundColor = UIColor.red
+    }
+
+    func clearAllStrings() {
+        for string in strings {
+            clearString(string)
+        }
+    }
+
+    func clearString(_ string: [UIButton]) {
+        for fret in string {
+            fret.backgroundColor = #colorLiteral(red: 0.8761691451, green: 0.5170841813, blue: 0.1610716283, alpha: 1)
+        }
+    }
+
+    func initStrings() {
+        clearAllStrings()
+    }
 }
+
 
---
+++ GuitarChordsBrain.swift
@@ -0,0 +1,18 @@
+//
+//  GuitarChordsBrain.swift
+//  GuitarChords
+//
+//  Created by  Robin Smith on 16/11/2017.
+//  Copyright © 2017  Robin Smith. All rights reserved.
+//
+
+import Foundation
+
+
+struct GuitarChordsBrain {
+    private enum Chord {
+        case c("C")
+    }
+
+    
+}
